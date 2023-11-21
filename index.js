3979a045c1ad5dcaf5039da558344bd5922e5f6d
ababacar cisse <ababacarcisse18@gmail.com>
2023-11-21 22:22:13 +0100

    lear dom js

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ divCarte.classList.add('carte');
 const balise ="<h2>how are you </h2>";
 divCarte.innerHTML = balise;
 main.append(divCarte);
+const titres=document.querySelectorAll('h1');
+console.log(titres);
+for (let titre = 0; titre < array.length; titre++) {
+    const element = array[titre];
+    console.log(element);
+}
+
